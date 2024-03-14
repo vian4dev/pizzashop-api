cf082c52e5710aefb1969ed4cd36628623477a8f
Gabriel Viana <viannadoze@gmail.com>
2024-03-14 16:44:09 -0300

    Criando API com Bun e Elysia

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+
+.get('/', () => {
+    return 'Hello World'
+})
+
+app.listen(3333, () => {
+    console.log('🔥 HTTP server running')
+})
