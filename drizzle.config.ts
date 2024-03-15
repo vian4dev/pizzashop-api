a5517f6a1e9f16a3ec0bebc5b5f980b6b87e39dd
Gabriel Viana <viannadoze@gmail.com>
2024-03-15 14:22:13 -0300

    Migrations no Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: 'pg',
+  dbCredentials: {
+    connectionString: 'postgres://docker:docker@localhost:5432/pizzashop'
+  }
 } satisfies Config;
