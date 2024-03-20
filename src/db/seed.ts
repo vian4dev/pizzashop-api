0ba57f0f129f43b7523467927be3df4dc85c9e31
Gabriel Viana <viannadoze@gmail.com>
2024-03-19 22:39:58 -0300

    Criando seed com Drizzle

---
+++ src/db/seed.ts
@@ -0,0 +1,62 @@
+import { faker } from '@faker-js/faker';
+import { users, restaurants } from './schema'
+import { db } from './connection'
+import chalk from 'chalk'
+
+/**
+ * reset database
+ */
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellow('✔️ Database reset!'))
+
+/**
+ * Create customers
+ */
+await db.insert(users).values([
+    {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        role: 'customer',
+    },
+    {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        role: 'customer',
+    }
+])
+
+console.log(chalk.yellowBright('✔️ Created customers!'))
+
+/**
+ * Create manager
+ */
+const [manager] = await db.insert(users).values([
+    {
+        name: faker.person.fullName(),
+        email: 'admin@admin.com',
+        role: 'manager',
+    },
+]).returning({
+    id: users.id,
+})
+
+console.log(chalk.yellowBright('✔️ Created manager!'))
+
+/**
+ * Create restaurante
+ */
+await db.insert(restaurants).values([
+    {
+        name: faker.company.name(),
+        description: faker.lorem.paragraph(),
+        managerId: manager.id,
+    },
+])
+
+console.log(chalk.yellowBright('✔️ Created manager!'))
+
+console.log(chalk.greenBright('Database seeded successfully!'))
+
+process.exit()
