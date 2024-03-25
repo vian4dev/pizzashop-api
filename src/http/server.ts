f94aa4c64344cf53db77906c73dc4724b94375ba
Gabriel Viana <viannadoze@gmail.com>
2024-03-24 23:38:57 -0300

    Setup da Autenticação JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,19 @@
 import { Elysia, t } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
-import { authLinks } from '../db/schema'
 import { sendAuthlink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
 const app = new Elysia()
+  .use(jwt({
+    secret: env.JWT_SECRET_KEY,
+    schema: t.Object({
+      sub: t.String(),
+      restaurant: t.Optional(t.String()),
+    }),
+  }))
+  .use(cookie())
   .use(registerRestaurant)
   .use(sendAuthlink)
 
