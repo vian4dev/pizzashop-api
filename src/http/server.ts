dba33381c82f8248a781e4665db1047601fa9e2f
Gabriel Viana <viannadoze@gmail.com>
2024-03-25 16:56:53 -0300

    Rota: Autenticação pelo link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,21 +1,12 @@
 import { Elysia, t } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthlink } from './routes/send-auth-link'
-import jwt from '@elysiajs/jwt'
-import { env } from '../env'
-import cookie from '@elysiajs/cookie'
+import { authenticateFromLink } from './routes/authenticated-from-link'
 
 const app = new Elysia()
-  .use(jwt({
-    secret: env.JWT_SECRET_KEY,
-    schema: t.Object({
-      sub: t.String(),
-      restaurant: t.Optional(t.String()),
-    }),
-  }))
-  .use(cookie())
   .use(registerRestaurant)
   .use(sendAuthlink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running')
